c7aae9f53059d955a5bf872f7702c1db98a60312
DipStax <DipStax.hugo@gmail.com>
2025-06-02 17:49:37 +0200

    feat(client): add QT dependency

--- client/GUI/src/main.cpp
+++ client/GUI/src/main.cpp
@@ -1,4 +1,12 @@
-int main()
+#include <QApplication>
+#include <QMainWindow>
+
+int main(int argc, char *argv[])
 {
-    
+    QApplication app(argc, argv);
+    QMainWindow window;
+
+    window.setWindowTitle("My Qt App");
+    window.show();
+    return app.exec();
 }
