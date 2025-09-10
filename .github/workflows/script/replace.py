dcb5e8542e889013a4f053457df7920621f5207f
DipStax <DipStax.hugo@gmail.com>
2025-09-10 23:09:04 +0200

    feat(CI): replace variable

---
+++ .github/workflows/script/replace.py
@@ -0,0 +1,37 @@
+import sys
+import os
+import json
+import re
+
+from typing import List, Dict, Any
+
+def replace(text: str, vars_map: Dict[str, Any]) -> str:
+    pattern = re.compile(r"\{\{\s*([A-Za-z0-9_]+)\s*\}\}")
+
+    def replacer(match):
+        var_name = match.group(1)
+        return str(vars_map.get(var_name, match.group(0)))  # fallback to original
+
+    return pattern.sub(replacer, text)
+
+def main(arg: List[str]) -> int:
+    vars_map = json.loads(os.getenv('VARS_JSON'))
+
+    for filename in arg:
+        with open(filename, 'r') as file:
+            content: str = file.read()
+
+        replaced = replace(content, vars_map)
+
+        with open(filename, "w") as file:
+            file.write(replaced)
+
+        print(f"Processed {filename}")
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage:")
+        print("\tpython replace.py <file1> [<file2> ...]")
+        sys.exit(1)
+
+    sys.exit(main(sys.argv[1:]))
