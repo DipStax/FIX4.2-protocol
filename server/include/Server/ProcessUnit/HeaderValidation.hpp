bb24bee24c9acaa63e44fcf7c132572801007c22
DipStax <DipStax.hugo@gmail.com>
2025-09-13 22:06:56 +0200

    doc(Server): header validation PU

--- server/include/Server/ProcessUnit/HeaderValidation.hpp
+++ server/include/Server/ProcessUnit/HeaderValidation.hpp
@@ -13,13 +13,22 @@ namespace pu
             virtual ~HeaderValidation() = default;
 
         protected:
+            /// @brief Verify all the value contained in the header
+            /// @param _input Input to process
             void onInput(InputType _input) final;
 
+            /// @brief Verify value of the header link to the positional tag
+            /// @param _header Header to verify
+            /// @return Return a ready to send `SessionReject` if `BeginString` or `MsgType` tag have invalid value
             [[nodiscard]] std::optional<fix42::msg::SessionReject> verifyPositionValue(const fix42::Header &_header);
+            /// @brief Verify value of the header related to the user if login
+            /// @param _header Header to verify
+            /// @param _client Client related to the header/message received
+            /// @return Return a ready to send `SessionReject` if `TargetCompId`, `SenderCompId` or `MsgSeqNum` tag have invalid value
             [[nodiscard]] std::optional<fix42::msg::SessionReject> verifyUserSpecific(const fix42::Header &_header, const ClientStore::Client &_client);
 
         private:
-            UnparsedMessageQueue &m_output;
-            StringOutputQueue &m_error;
+            UnparsedMessageQueue &m_output;             ///< Standard output for incoming message
+            StringOutputQueue &m_error;                 ///< Error output to TCP/IP network output
     };
 }
